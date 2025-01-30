5692ad4e90ec0e56d490316ab7ee6d80ba79536f
lexsilva1 <jagmsilva@student.dei.uc.pt>
2025-01-30 00:34:38 +0000

    postgres update to docker build

---
+++ app/app/calc.py
@@ -0,0 +1,18 @@
+"""
+Calculator Functions
+"""
+
+
+
+def add(x, y):
+    """
+    Add two numbers together
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    Subtract x from y and return value
+    """
+    return y - x
